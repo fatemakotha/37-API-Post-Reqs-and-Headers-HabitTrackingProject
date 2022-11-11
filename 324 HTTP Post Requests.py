1e982b8fb421c6acb598ab03844e33134cb74600
fatemakotha <fatema.alam.kotha@gmail.com>
2022-11-11 15:48:36 +0600

    Set up user account

--- 324 HTTP Post Requests.py
+++ 324 HTTP Post Requests.py
@@ -3,3 +3,17 @@
 # POST: requests.post() is where we give the ext system some piece of data & we are not interested in the response other than whether it was successful
 # PUT: requests.put() is where we update a piece of data in the ext service i.e. lets say you want to update sth in a spreadcheet
 # DELETE: requests.delete() is where we delete a piece of data from ext ervice i.e. delete a fb post
+
+import requests
+
+pixela_endpoint = "https://pixe.la/v1/users"
+
+user_params = {
+    "token": "jh5jh5jh5jjh5jh5", #set yourself
+    "username": "fatemakotha", #set yourself
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes",
+}
+#Create a new user in pixela
+response = requests.post(url=pixela_endpoint, json=user_params)
+print(response.text) #PRINTS: {"message":"Success. Let's visit https://pixe.la/@fatemakotha , it is your profile page!","isSuccess":true}
