41632a2a6e86e0f55e4fc4c4261db7fe1970415c
fatemakotha <fatema.alam.kotha@gmail.com>
2022-11-11 16:07:32 +0600

    Create graph

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import requests
 
+USERNAME = "fatemakotha"
+TOKEN = "jh5jh5jh5jjh5jh5"
 pixela_endpoint = "https://pixe.la/v1/users"
+
 
 user_params = {
     "token": "jh5jh5jh5jjh5jh5", #set yourself
@@ -9,5 +12,23 @@ user_params = {
     "notMinor": "yes",
 }
 #Create a new user
-response = requests.post(url=pixela_endpoint, json=user_params)
-print(response.text) #PRINTS: {"message":"Success. Let's visit https://pixe.la/@fatemakotha , it is your profile page!","isSuccess":true}
+# response = requests.post(url=pixela_endpoint, json=user_params)
+# print(response.text) #PRINTS: {"message":"Success. Let's visit https://pixe.la/@fatemakotha , it is your profile page!","isSuccess":true}
+
+#..........................................................................................................................................................
+
+pixela_endpoint_create_graph = f"{pixela_endpoint}/{USERNAME}/graphs"
+create_graph_params = {
+    "id": "graph1",
+    "name": "Commitment Graph",
+    "unit": "Commit",
+    "type": "int",
+    "color": "kuro",
+}
+
+headers = {
+    "X-USER-TOKEN": "jh5jh5jh5jjh5jh5",
+}
+
+create_graph_response = requests.post(url=pixela_endpoint_create_graph, json=create_graph_params, headers=headers)
+print(create_graph_response.text)
