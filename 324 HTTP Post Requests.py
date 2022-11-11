41b8cce38a522979cf6f3dae2e9f34fbb924275c
fatemakotha <fatema.alam.kotha@gmail.com>
2022-11-11 15:37:36 +0600

    Get Post Put Delete

---
+++ 324 HTTP Post Requests.py
@@ -0,0 +1,5 @@
+# Types of HTTP Requests:
+# GET: requests.get() is made where we ask an external system for a particular piece of data in the form of response
+# POST: requests.post() is where we give the ext system some piece of data & we are not interested in the response other than whether it was successful
+# PUT: requests.put() is where we update a piece of data in the ext service i.e. lets say you want to update sth in a spreadcheet
+# DELETE: requests.delete() is where we delete a piece of data from ext ervice i.e. delete a fb post
